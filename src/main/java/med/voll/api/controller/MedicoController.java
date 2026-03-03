437d2dceb8c313931beca869a76f877bc4d7aeb1
Giscelmo <giscelmo.costa@gmail.com>
2026-03-03 13:56:21 -0300

    refactor(controller): update controllers after exception handling and package reorganization

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,7 +21,7 @@ public class MedicoController {
     @PostMapping
     @Transactional // USADO QUANDO VAI FAZER MODIFICAÇÕES NO BANCO DE DADOS
     public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
-        var medico = new  Medico(dados);
+        var medico = new Medico(dados);
         repository.save(medico);
 
         // CRIA A URL
